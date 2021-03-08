c188d56ebf6533bf401ebcc220946944983eb86b
propgmachine <77746698+propgmachine@users.noreply.github.com>
2021-03-08 14:58:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,45 @@
 
 
 
+function isItLong(str) {
+  if (str.length > 20) {
+    return "That's a long string!";
+  }
+}
 
+function isItMedium(str) {
+  if (str.length >= 10 && str.length <= 20) {
+    return "That's a regular sized string!";
+  }
+}
 
+function isItShort(str) {
+  if (str.length < 10) {
+    return "That's a small string!";
+  } else {
+    return "That's not a small string!";
+  }
+}
+
+function howLongIsMyString(str) {
+  if (str.length > 20) {
+    return "That's a long string!";
+  } else if (str.length >= 10 && str.length <= 20) {
+    return "That's a regular sized string!";
+  } else {
+    return "That's a small string!";
+  }
+}
+
+function instructorHeight(instructorName) {
+  if (instructorName === "Colin") {
+    return 62;
+  } else if (instructorName === "Mesuara") {
+    return 69
+  } else {
+    return "I don't know that instructor!";
+  }
+}
 /**********************************
  *      OUR CODE GOES BELOW.      *
  * THIS IS FOR INTERNAL USE ONLY. *
